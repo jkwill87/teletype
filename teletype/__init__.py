71bb9bb4e67b1e67d01a335919458d5bd52e1def
Jessy Williams <jwilliams@freshbooks.com>
2019-05-23 19:00:19 -0400

    Major version bump

--- teletype/__init__.py
+++ teletype/__init__.py
@@ -1,9 +1,7 @@
-# coding=utf-8
-
 from os import name as os_name
 
 __all__ = ["VERSION", "IS_WINDOWS"]
 
-VERSION = "0.3.1"
+VERSION: str = "1.0.0"
 
-IS_WINDOWS = os_name in ("nt", "cygwin")
+IS_WINDOWS: bool = os_name in ("nt", "cygwin")
